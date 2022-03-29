8ba90e4ac20aba8f2e2238e1fd8327510c46f6ab
francescocaredda <francecaredda6@gmail.com>
2022-03-29 13:44:30 +0200

    added compute_ppv

--- src/plmdca_asym.jl
+++ src/plmdca_asym.jl
@@ -255,3 +255,42 @@ function ComputeScore(Jmat::Array{Float64,2}, var::PlmVar, min_separation::Int)
     score = compute_ranking(FN, min_separation)
     return score, FN, Jplm, hplm
 end
+
+
+#Aggiunto da me
+using DelimitedFiles
+function compute_PPV(filestruct::String, score; N=53 min_separation::Int=1, verbose=true, kwds...) 
+    dist = compute_residue_pair_dist(filestruct)
+    roc = compute_referencescore(score, dist; kwds...)
+    verbose && println("PPV(N) = ", roc[N][end], " PPV(N/5) = ", roc[div(N,5)][end])
+    return roc
+end
+
+function compute_residue_pair_dist(filedist::String)
+    d = readdlm(filedist)
+    return Dict((round(Int,d[i,1]),round(Int,d[i,2])) => d[i,4] for i in 1:size(d,1))
+end
+
+function compute_referencescore(score,dist::Dict; mindist::Int=4, cutoff::Number=7.0)
+    nc2 = length(score)
+    #nc2 == size(d,1) || throw(DimensionMismatch("incompatible length $nc2 $(size(d,1))"))
+    out = Tuple{Int,Int,Float64,Float64}[]
+    ctrtot = 0
+    ctr = 0
+    for i in 1:nc2
+        sitei,sitej,plmscore = score[i][1],score[i][2], score[i][3]
+        dij = if haskey(dist,(sitei,sitej)) 
+            dist[(sitei,sitej)]
+        else
+           continue
+        end
+        if sitej - sitei > mindist
+            ctrtot += 1
+            if dij < cutoff
+                ctr += 1
+            end
+            push!(out,(sitei,sitej, plmscore, ctr/ctrtot))
+        end
+    end 
+    out
+end
